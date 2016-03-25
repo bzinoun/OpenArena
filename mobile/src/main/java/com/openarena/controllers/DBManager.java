8d38e8d857dd1c64e10cdb741ca46c88b294a6f0
TarikW <decaprio911@gmail.com>
2016-03-25 14:46:53 +0200

    Add 2 method for work with Fixture

--- mobile/src/main/java/com/openarena/controllers/DBManager.java
+++ mobile/src/main/java/com/openarena/controllers/DBManager.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 
 import com.openarena.model.SQLHelper;
+import com.openarena.model.objects.Fixture;
 import com.openarena.model.objects.League;
 import com.openarena.util.DBConst;
 
@@ -14,6 +15,26 @@ public class DBManager {
 
 	public void init(Context context) {
 		sSQLHelper = SQLHelper.getInstance(context);
+	}
+
+	public static League getLeague(int id) {
+		Cursor cursor = sSQLHelper.getAll(
+				DBConst.TABLE_LEAGUES,
+				new String[] {DBConst.ID},
+				new String[] {String.valueOf(id)});
+		League league = League.parse(cursor);
+		if (cursor != null) cursor.close();
+		return league;
+	}
+
+	public static Fixture getFixture(int id) {
+		Cursor cursor = sSQLHelper.getAll(
+				DBConst.TABLE_FIXTURES,
+				new String[] {DBConst.ID},
+				new String[] {String.valueOf(id)});
+		Fixture fixture = Fixture.parse(cursor);
+		if (cursor != null) cursor.close();
+		return fixture;
 	}
 
 	public static void setLeague(League league) {
@@ -46,14 +67,36 @@ public class DBManager {
 		}
 	}
 
-	public static League getLeague(int id) {
-		Cursor cursor = sSQLHelper.getAll(
-				DBConst.TABLE_LEAGUES,
-				new String[] {DBConst.ID},
-				new String[] {String.valueOf(id)});
-		League league = League.parse(cursor);
-		if (cursor != null) cursor.close();
-		return league;
+	public static void setFixture(Fixture fixture) {
+		if (fixture != null) {
+			ContentValues data = new ContentValues();
+			data.put(DBConst.ID, fixture.getID());
+			data.put(DBConst.SOCCER_SEASON_ID, fixture.getSoccerSeasonID());
+			data.put(DBConst.DATE, fixture.getDate());
+			data.put(DBConst.STATUS, fixture.getStatus());
+			data.put(DBConst.MATCHDAY, fixture.getMatchday());
+			data.put(DBConst.HOME_TEAM_ID, fixture.getHomeTeamID());
+			data.put(DBConst.HOME_TEAM_NAME, fixture.getHomeTeamName());
+			data.put(DBConst.AWAY_TEAM_ID, fixture.getAwayTeamID());
+			data.put(DBConst.AWAY_TEAM_NAME, fixture.getAwayTeamName());
+			data.put(DBConst.GOALS_HOME_TEAM, fixture.getGoalsHomeTeam());
+			data.put(DBConst.GOALS_AWAY_TEAM, fixture.getGoalsAwayTeam());
+
+			Cursor cursor = sSQLHelper.getAll(
+					DBConst.TABLE_FIXTURES,
+					new String[] {DBConst.ID},
+					new String[] {String.valueOf(fixture.getID())});
+			if (cursor.moveToFirst()) {
+				sSQLHelper.update(
+						DBConst.TABLE_FIXTURES,
+						data, new String[] {DBConst.ID},
+						new String[] {String.valueOf(fixture.getID())});
+			}
+			else {
+				sSQLHelper.insert(DBConst.TABLE_FIXTURES, data);
+			}
+			cursor.close();
+		}
 	}
 
 }
