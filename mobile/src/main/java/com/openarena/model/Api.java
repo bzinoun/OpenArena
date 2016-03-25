3467c5ea12dfb4f604272997a2e1acdeb441f63f
TarikW <decaprio911@gmail.com>
2016-03-25 14:43:21 +0200

    Add API method to get Fixtures by soccerseason id

--- mobile/src/main/java/com/openarena/model/Api.java
+++ mobile/src/main/java/com/openarena/model/Api.java
@@ -7,9 +7,30 @@ import com.openarena.controllers.Connection;
 
 public class Api {
 
-	public static String getLeaguesList(Context context, int year) {
+	public static String getLeagueByYear(Context context, int year) {
 		Connection connection = getConnection(context);
 		return connection.request(getBaseUri() + "soccerseasons?season=" + String.valueOf(year));
+	}
+
+	public static String getFixturesBySeasonId(Context context, int id) {
+		return getFixturesBySeasonId(context, id, null);
+	}
+
+	/**
+	 *
+	 * @param timeFrame <b><i>//p|n[1-9]{1,2}//</i></b> of <b>null</b> for example "n1"
+	 */
+	public static String getFixturesBySeasonId(Context context, int id, String timeFrame) {
+		Connection connection = getConnection(context);
+		StringBuilder request = new StringBuilder()
+				.append(getBaseUri())
+				.append("soccerseasons/")
+				.append(String.valueOf(id))
+				.append("/fixtures");
+		if (timeFrame != null) request.append("?timeFrame=")
+				.append(timeFrame);
+		else request.append("?timeFrame=n1");
+		return connection.request(request.toString());
 	}
 
 	private static Connection getConnection(Context context) {
